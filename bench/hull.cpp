c0d6f853eb454a311a4928ee46b11a3184a3a2cc
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-01-29 20:48:55 +0100

    got hull to pass initial correctness check

--- bench/hull.cpp
+++ bench/hull.cpp
@@ -20,24 +20,41 @@ parray<pbbs::_point2d<double>> to_pbbs(parray<sptl::_point2d<double>>& points) {
 }
 
 void pbbs_sptl_call(sptl::bench::measured_type measured, parray<sptl::_point2d<double>>& x) {
-  std::string lib_type = deepsea::cmdline::parse_or_default_string("lib_type", "pctl");
-  if (lib_type == "pbbs") {
+  deepsea::cmdline::dispatcher d;
+  pbbs::_seq<intT> pbbs_result;
+  auto do_pbbs = [&] {
     parray<pbbs::_point2d<double>> y = to_pbbs(x);
     measured([&] {
-      pbbs::hull(&y[0], (int)y.size());
+      pbbs_result = pbbs::hull(&y[0], (int)y.size());
     });
-  } else {
+  };
+  d.add("pbbs", do_pbbs);
+  d.add("pctl", [&] {
+    parray<intT> sptl_result;
     measured([&] {
-      sptl::hull(x);
+      sptl_result = sptl::hull(x);
     });
-  }
+    if (deepsea::cmdline::parse_or_default_bool("check", false)) {
+      do_pbbs();
+      if (pbbs_result.n != sptl_result.size()) {
+        sptl::die("bogus size");
+      }
+      for (size_t i = 0; i < sptl_result.size(); i++) {
+        if (sptl_result[i] != pbbs_result.A[i]) {
+          sptl::die("bogus item at position %d", i);
+        }
+      }
+    }
+  });
+  d.dispatch("library");
+  pbbs_result.del();
 }
 
 int main(int argc, char** argv) {
   sptl::bench::launch(argc, argv, [&] (sptl::bench::measured_type measured) {
     std::string infile = deepsea::cmdline::parse_or_default_string("infile", "");
     if (infile == "") {
-      sptl::die("bogus infile");
+      sptl::die("missing infile");
     }
     parray<sptl::_point2d<double>> x = sptl::read_from_file<parray<sptl::_point2d<double>>>(infile);
     pbbs_sptl_call(measured, x);
