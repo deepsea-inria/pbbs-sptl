72e40e7d6bb5d394515228551e350650d1a34b27
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-02-04 19:49:55 +0100

    introducing mis benchmark

---
+++ bench/mis.cpp
@@ -0,0 +1,58 @@
+
+#include <math.h>
+#include <functional>
+#include <stdlib.h>
+#include <algorithm>
+
+#include "bench.hpp"
+
+#include "mis.hpp"
+#include "mis.h"
+
+template <class Item>
+using parray = sptl::parray<Item>;
+
+void benchmark(sptl::bench::measured_type measured) {
+  std::string infile = deepsea::cmdline::parse_or_default_string("infile", "");
+  if (infile == "") {
+    sptl::die("missing infile");
+  }
+  sptl::graph::graph<intT> x = sptl::read_from_file<sptl::graph::graph<intT>>(infile);
+  bool should_check = deepsea::cmdline::parse_or_default_bool("check", false);
+  parray<char> sptl_results;
+  char* pbbs_results = nullptr;
+  auto do_pbbs = [&] {
+    parray<pbbs::graph::vertex<int>> vs(x.n, [&] (int i) {
+      return pbbs::graph::vertex<int>(x.V[i].Neighbors, x.V[i].degree);
+    });
+    pbbs::graph::graph<intT> y(vs.begin(), x.n, x.m, x.allocatedInplace);
+    measured([&] {
+      pbbs_results = pbbs::maximalIndependentSet(y);
+    });
+  };
+  deepsea::cmdline::dispatcher d;
+  d.add("pbbs", do_pbbs);
+  d.add("sptl", [&] {
+    measured([&] {
+      sptl_results = sptl::maximalIndependentSet(x);
+    });
+    if (should_check) {
+      do_pbbs();
+      for (intT i = 0; i < sptl_results.size(); i++) {
+        if (sptl_results[i] != pbbs_results[i]) {
+          sptl::die("bogus result at index %d\n", i);
+        }
+      }
+    }
+  });
+  d.dispatch("library");
+  if (pbbs_results != nullptr) {
+    free(pbbs_results);
+  }
+}
+
+int main(int argc, char** argv) {
+  sptl::bench::launch(argc, argv, [&] (sptl::bench::measured_type measured) {
+    benchmark(measured);
+  });
+}
---
+++ include/mis.hpp
@@ -0,0 +1,76 @@
+// This code is part of the Problem Based Benchmark Suite (PBBS)
+// Copyright (c) 2011 Guy Blelloch and the PBBS team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights (to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <iostream>
+
+#include "graph.hpp"
+#include "speculativefor.hpp"
+
+#ifndef MIS_H_
+#define MIS_H_
+
+namespace sptl {
+    
+
+// **************************************************************
+//    MAXIMAL INDEPENDENT SET
+// **************************************************************
+
+// For each vertex:
+//   Flags = 0 indicates undecided
+//   Flags = 1 indicates chosen
+//   Flags = 2 indicates a neighbor is chosen
+struct MISstep {
+  char flag;
+  char* flags;  graph::vertex<int>* G;
+  MISstep() { }
+  MISstep(char* _F, graph::vertex<int>* _G) : flags(_F), G(_G) {}
+  
+  bool reserve(intT i) {
+    intT d = G[i].degree;
+    flag = 1;
+    for (intT j = 0; j < d; j++) {
+      intT ngh = G[i].Neighbors[j];
+      if (ngh < i) {
+        if (flags[ngh] == 1) { flag = 2; return 1;}
+        // need to wait for higher priority neighbor to decide
+        else if (flags[ngh] == 0) flag = 0;
+      }
+    }
+    return 1;
+  }
+  
+  bool commit(intT i) { return (flags[i] = flag) > 0;}
+};
+
+parray<char> maximalIndependentSet(graph::graph<int> GS) {
+  intT n = GS.n;
+  graph::vertex<int>* G = GS.V;
+  parray<char> flags(n, (char) 0);
+  MISstep mis(flags.begin(), G);
+  speculative_for(mis, 0, n, 20);
+  return flags;
+}
+  
+} // end namespace
+
+#endif
