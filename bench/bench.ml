f277c8d33bbb64194d8575f5d984131e6128d346
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-02-05 00:48:37 +0100

    introducing bfs benchmark script

--- bench/bench.ml
+++ bench/bench.ml
@@ -130,6 +130,274 @@ let string_of_percentage_change ?(show_plus=true) vold vnew =
   string_of_percentage ~show_plus:show_plus (vnew /. vold -. 1.0)
 
 (*****************************************************************************)
+(** BFS benchmark *)
+
+module ExpBFS = struct
+
+let name = "bfs"
+
+let results_file = "_results/results_bfs.txt" 
+                   
+let graphfile_of n = "_data/" ^ n ^ ".bin"
+
+let graphfiles' =
+  let manual = 
+    [
+      "livejournal", 0;
+      "twitter", 1;
+      (*        "usa", 1;*)
+    ]
+  in
+  let other =
+    [
+      "wikipedia-20070206"; (*"rgg";*) (*"delaunay";*) "europe"; 
+      "random_arity_100_large"; "rmat27_large"; 
+      "rmat24_large";  "cube_large"; "grid_sq_large";
+      "paths_100_phases_1_large"; "unbalanced_tree_trunk_first_large";
+      "phased_mix_10_large"; (*"tree_2_512_1024_large";*) "phased_low_50_large";
+      "phased_524288_single_large"; 
+    ]
+  in
+  List.concat [manual; List.map (fun n -> (n, 0)) other]
+
+let mk_proc = mk int "proc" (List.hd (List.rev arg_proc))
+    
+let user_graph_argument = XCmd.parse_optional_list_string "graph"
+
+let graphfiles' = match user_graph_argument with
+  | None -> graphfiles'
+  | Some s -> List.map (fun s -> (s, List.assoc s graphfiles')) s
+              
+let graphfiles = List.map (fun (n, _) -> n) graphfiles'
+
+let graph_renaming =
+  [
+   "grid_sq_large", "square-grid";
+   "wikipedia-20070206", "wikipedia";
+   "paths_100_phases_1_large", "par-chains-100";
+   "phased_524288_single_large", "trees-524k";
+   "phased_low_50_large", "phases-50-d-5";
+   "phased_mix_10_large", "phases-10-d-2";
+   "random_arity_100_large", "rand-arity-100";
+   "tree_2_512_1024_large", "trees-512-1024";
+   "unbalanced_tree_trunk_first_large", "trunk-first";
+   "randLocalGraph_J_5_10000000", "random";
+   "rMatGraph_J_5_10000000", "rMat";
+   "cube_large", "cube-grid";
+   "rmat24_large", "rmat24";
+   "rmat27_large", "rmat27";
+ ]
+
+let pretty_graph_name n =
+  if List.mem_assoc n graph_renaming then
+    List.assoc n graph_renaming
+  else
+    n
+
+let extensions = XCmd.parse_or_default_list_string "exts" [ "sptl"; ]
+
+let arg_inner_loop = XCmd.parse_or_default_list_string "inner_loop" ["bfs";"pbfs"]
+
+let prog benchmark extension =
+  sprintf "%s.%s" benchmark extension
+
+let baseline_progs = List.map (fun inner_loop -> prog inner_loop "sptl") arg_inner_loop
+
+let oracle_progs = List.flatten 
+  (List.map (fun extension -> List.map (fun inner_loop -> prog inner_loop extension) arg_inner_loop) extensions)
+
+let all_progs = List.append baseline_progs oracle_progs                       
+
+let make() =
+  build "." all_progs arg_virtual_build
+
+let mk_lib_type t =
+  mk string "library" t
+
+let mk_infile n =
+  mk string "infile" n
+
+let mk_infile' n =
+  let (_, source) = List.find (fun (m, _) -> m = n) graphfiles' in
+  mk string "infile" (graphfile_of n) & mk int "source" source
+
+let mk_graphname n =
+  mk string "graph_name" n
+
+let mk_input n =
+  (mk_infile' n & mk_graphname n)
+
+let rec mk_infiles ns =
+  match ns with
+  | [] ->
+      failwith ""
+  | [n] ->
+      mk_input n
+  | n :: ns ->
+      mk_input n ++ mk_infiles ns
+
+let prog_assoc = List.flatten (List.map (fun (p:string) -> 
+                               List.map (fun (e:string) -> (p, e)) extensions) arg_inner_loop)
+
+let prog_of (p, e) = sprintf "%s_bench.%s" p e
+
+let mk_bfs_prog inner_loop extension lib_type =
+  (mk string "prog" (prog inner_loop extension)) & (mk_lib_type lib_type)
+
+let mk_progs =
+  ((mk_list string "prog" oracle_progs)  & (mk_lib_type "pctl")) ++
+    ((mk_list string "prog" baseline_progs)  & (mk_lib_type "pbbs"))
+
+let run() =
+  Mk_runs.(call (run_modes @ [
+                    Output results_file;
+                    Timeout 400;
+                    Args (mk_progs & (mk string "type" "graph") & (mk_infiles graphfiles) & mk_proc);                           
+                  ]))
+
+let check = nothing  (* do something here *)
+
+let main_formatter =
+  Env.format (Env.(
+              [
+               ("proc", Format_custom (fun n -> ""));
+               ("lib_type", Format_custom (fun n -> ""));
+               ("infile", Format_custom (fun n -> ""));
+               ("graph_name", Format_custom pretty_graph_name);
+               ("prog", Format_custom (fun n ->  ""
+(*                                           let ps = List.map2 (fun x y -> (x, y)) oracle_progs prog_assoc in
+                                         if List.mem_assoc n ps then
+                                           let (p, e) = List.assoc n ps in
+                                           let commonext = "unks" in
+                                           let extlength = String.length commonext in
+                                           let kappa = String.sub e extlength (String.length e - extlength) in
+                                           let nghl = if p = "bfs" then "Seq. ngh. list" else "Par. ngh. list" in
+                                           sprintf "Oracle guided, kappa := %sus (%s)" kappa nghl
+                                         else "<bogus>" *)
+                                      ));    
+               ("type", Format_custom (fun n -> ""));
+               ("source", Format_custom (fun n -> ""));
+             ]
+               ))
+
+let eval_relative_main = fun env all_results results ->
+  let pbbs_results = ~~ Results.filter_by_params all_results (
+                          from_env (Env.add (Env.filter_keys ["type"; "infile"; "proc"] env) "lib_type" (Env.Vstring "pbbs"))) in
+  if pbbs_results = [] then Pbench.error ("no results for pbbs library");
+  let v = Results.get_mean_of "exectime" results in
+  let b = Results.get_mean_of "exectime" pbbs_results in
+  (b, v)
+
+let plot() =
+  let pretty_extension ext =
+    let l = String.length ext in
+    let plen = 4 in
+    if l < plen then
+      "<unknown extension>"
+    else
+      "Ours"
+(*      let p = String.sub ext 0 plen in
+      let mu = int_of_string (String.sub ext plen (l - plen)) in
+      sprintf "{\\begin{tabular}[x]{@{}c@{}}Ours\\\\($\kappa$ := %d%ssec.)\\end{tabular}}" mu "$\\mu$" *)
+  in
+
+  let nb_inner_loop = List.length arg_inner_loop in
+  let tex_file = file_tables_src name in
+  let pdf_file = file_tables name in
+  Mk_table.build_table tex_file pdf_file (fun add ->
+(*    let l = "S[table-format=2.2]" in*) (* later: use *)
+    (*let ls = String.concat "|" (XList.init (2 * nb_inner_loop + 2) (fun _ -> "d{3.2}")) in*)
+    let ls = "c|d{3.2}|c|d{3.2}|@{}d{3.2}@{}" in
+    let hdr = Printf.sprintf "@{}l@{\,}|%s@{}" ls in
+    add (Latex.tabular_begin hdr);                                    
+    let _ = Mk_table.cell ~escape:false ~last:false add "" in
+    ~~ List.iteri arg_inner_loop (fun i inner_loop ->
+          let last = false (* i + 1 = nb_inner_loop*) in
+          let n = "{" ^ (if inner_loop = "bfs" then "Flat" else "Nested") ^ "}" in
+          let l = if last then "c" else "c|" in
+          let label = Latex.tabular_multicol 2 l n in
+          Mk_table.cell ~escape:false ~last:last add label);
+    (* let label = Latex.tabular_multicol 2 "c" "" in *)
+    let label = Latex.tabular_multicol 1 "c" "Ours nested" in 
+    Mk_table.cell ~escape:false ~last:true add label;
+    (*add Latex.tabular_newline;*)
+    add "\\\\ \cline{1-5}";
+    let _ = Mk_table.cell ~escape:false ~last:false add "Graph" in
+    for i=1 to nb_inner_loop do (
+      let l = "\multicolumn{1}{@{\,}l@{\,}|}{{\\begin{tabular}[x]{@{}c@{}}PBBS\\\\(sec.)\\end{tabular}}}" in
+      Mk_table.cell ~escape:false ~last:false add l;
+      Mk_table.cell ~escape:false ~last:false add "\multicolumn{1}{l|}{Ours}")
+    done;
+    (*Mk_table.cell ~escape:false ~last:false add "\multicolumn{1}{l|}{{\\begin{tabular}[x]{@{}c@{}}PBBS Nested vs. \\\\PBBS Flat\\end{tabular}}}";*)
+    Mk_table.cell ~escape:false ~last:true add "\multicolumn{1}{@{}c@{}}{{\\begin{tabular}[x]{@{}c@{}}vs. \\\\PBBS flat\\end{tabular}}}";
+    (* Mk_table.cell ~escape:false ~last:true add "\multicolumn{1}{l}{{\\begin{tabular}[x]{@{}c@{}}Ours Nested vs. \\\\PBBS Flat\\end{tabular}}}";*)
+    add Latex.tabular_newline;
+        let all_results = Results.from_file results_file in
+        let results = all_results in
+        let env = Env.empty in
+        let env_rows = mk_infiles graphfiles env in
+        ~~ List.iter env_rows (fun env_rows ->  (* loop over each input for current benchmark *)
+          let results = Results.filter env_rows results in
+          let env = Env.append env env_rows in
+          let row_title = main_formatter env_rows in
+          let _ = Mk_table.cell ~escape:false ~last:false add row_title in
+          let exectime_bfs_pctl = ref 0.0 in
+          let exectime_pbfs_pctl = ref 0.0 in
+          let exectime_bfs_pbbs = ref 0.0 in
+          let exectime_pbfs_pbbs = ref 0.0 in
+          ~~ List.iteri arg_inner_loop (fun inner_loop_i inner_loop ->
+            let (pbbs_str, b) =
+              let [col] = (mk_bfs_prog inner_loop "manc" "pbbs") env in
+              let env = Env.append env col in
+              let results = Results.filter col results in
+              let b = eval_exectime env all_results results in
+              let _ = if inner_loop = "bfs" then (exectime_bfs_pbbs := b) else (exectime_pbfs_pbbs := b) in
+              let e = eval_exectime_stddev env all_results results in
+              let err = if arg_print_err then Printf.sprintf "(%.2f%s)" e "$\\sigma$" else "" in
+              let str = Printf.sprintf (if b < 10. then "%.2f" else "%.1f") b in
+              (str ^ " " ^ err, b)
+            in
+            let _ = Mk_table.cell ~escape:false ~last:false add pbbs_str in
+              let (pctl_str, grade_str) = 
+                let [col] = (mk_bfs_prog inner_loop "unks" "pctl") env in
+                let env = Env.append env col in
+                let results = Results.filter col results in
+                let (_,v) = eval_relative_main env all_results results in
+                let _ = if inner_loop = "bfs" then (exectime_bfs_pctl := v) else (exectime_pbfs_pctl := v) in
+                let vs = string_of_percentage_change b v in
+                let e = eval_exectime_stddev env all_results results in
+                let err = if arg_print_err then Printf.sprintf "(%.2f%s)" e "$\\sigma$" else "" in
+                let grade =
+                  let delta = v -. b in
+                  let grade = delta /. e in
+                  if (abs_float grade) < 0.51 then
+                    Printf.sprintf "$\\approx$"
+                  else if grade < 0.0 then
+                    Printf.sprintf "$\\checkmark^{%.0f}$" (abs_float grade)
+                  else
+                    Printf.sprintf "$\\times^{%.0f}$" grade
+                in
+                (Printf.sprintf "%s %s" vs err, grade)
+              in
+              Mk_table.cell ~escape:false add pctl_str;
+            ());
+          (*let str_diff_pbbs = string_of_percentage_change (!exectime_bfs_pbbs) (!exectime_pbfs_pbbs) in
+          Mk_table.cell ~escape:false ~last:false add str_diff_pbbs;*)
+          let str_diff_pctl = string_of_percentage_change (!exectime_bfs_pbbs) (!exectime_pbfs_pctl) in
+          Mk_table.cell ~escape:false ~last:true add str_diff_pctl;
+          add Latex.tabular_newline);
+        add Latex.tabular_end;
+        add Latex.new_page;
+        ());
+
+  ()
+
+
+let all () = select make run check plot
+
+end
+
+(*****************************************************************************)
 (** Comparison benchmark *)
 
 module ExpCompare = struct
@@ -140,8 +408,8 @@ let all_benchmarks =
   match arg_benchmarks with
   | ["all"] -> [
     "convexhull"; "samplesort"; "radixsort"; "nearestneighbors";
-    "suffixarray"; "removeduplicates"; "mis"; "mst"; "matching"; "spanning";
-    "delaunay"; "bfs"; (*"refine"; *) "raycast"; (*"pbfs";*)
+    "suffixarray"; "mis"; "mst"; "matching"; "spanning";
+    "delaunay"; (*"bfs";*) (*"refine"; *) "raycast"; (*"pbfs";*)
     ]
   | _ -> arg_benchmarks
     
@@ -350,18 +618,6 @@ let input_descriptor_raycast = List.map (fun (p, t, n) -> (path_to_infile p, t, 
 let mk_raycast_infiles = mk_infiles "type" input_descriptor_raycast    
 
 (*****************)
-(* Remove duplicates *)
-
-let input_descriptor_removeduplicates = List.map (fun (p, t, n) -> (path_to_infile p, t, n)) [
-  "array_int_random_large.bin", string "array_int", "random";
-  "array_int_random_bounded_100000_large.bin", string "array_int", "bounded random";
-  "array_int_exponential_large.bin", string "array_int", "exponential";
-  "array_string_trigrams_large.bin", string "array_string", "string trigrams";
-]
-
-let mk_removeduplicates_infiles = mk_infiles "type" input_descriptor_removeduplicates
-
-(*****************)
 (* All benchmarks *)
 
 let benchmarks' : benchmark_descriptor list = [
@@ -400,10 +656,6 @@ let benchmarks' : benchmark_descriptor list = [
   { bd_name = "suffixarray";
     bd_infiles = mk_suffixarray_infiles;
     bd_input_descr = input_descriptor_suffixarray;
-  };
-  { bd_name = "removeduplicates";
-    bd_infiles = mk_removeduplicates_infiles;
-    bd_input_descr = input_descriptor_removeduplicates;
   };
   { bd_name = "convexhull";
     bd_infiles = mk_hull_infiles;
@@ -677,6 +929,7 @@ end
 let _ =
   let arg_actions = XCmd.get_others() in
   let bindings = [
+    "bfs",     ExpBFS.all;
     "compare", ExpCompare.all;
   ]
   in
