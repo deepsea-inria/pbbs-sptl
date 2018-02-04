b09f8c95dcc8befc70e736e3aa5382c7b85c6638
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-02-04 01:39:41 +0100

    introducing preset loader by machine for kappa and alpha parameters

--- bench/bench.hpp
+++ bench/bench.hpp
@@ -1,4 +1,7 @@
 
+#include <unistd.h>
+#include <limits.h>
+
 #include "readinputbinary.hpp"
 
 #ifndef _PBBS_SPTL_BENCH_
@@ -10,6 +13,31 @@ namespace bench {
 using thunk_type = std::function<void()>;
 
 using measured_type = std::function<void(thunk_type)>;
+
+void load_presets_by_host() {
+  if (deepsea::cmdline::parse_or_default_bool("sptl_custom_kappa", false)) {
+    return;
+  }
+  char _hostname[HOST_NAME_MAX];
+  gethostname(_hostname, HOST_NAME_MAX);
+  std::string hostname = std::string(_hostname);
+  if (hostname == "teraram") {
+    kappa = 25.0;
+    update_size_ratio = 1.5;
+  } else if (hostname == "cadmium") {
+    kappa = 30.0;
+    update_size_ratio = 1.1;
+  } else if (hostname == "hiphi.aladdin.cs.cmu.edu") {
+    kappa = 40.0;
+    update_size_ratio = 1.2;
+  } else if (hostname == "aware.aladdin.cs.cmu.edu") {
+    kappa = 40.0;
+    update_size_ratio = 1.2;
+  } else if (hostname == "beast") {
+    kappa = 30.0;
+    update_size_ratio = 1.2;
+  }
+}
   
 template <class Body>
 void launch(int argc, char** argv, const Body& body) {
@@ -21,6 +49,7 @@ void launch(int argc, char** argv, const Body& body) {
     printf ("exectime %.3lf\n", diff.count());
   };
   sptl::launch(argc, argv, [&] {
+    load_presets_by_host();
     body(f);
   });
 }
