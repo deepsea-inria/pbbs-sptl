df5653e6ea775208a7e32dbe718c51edb12fa738
Umut Akgun Acar <umut@aware.aladdin.cs.cmu.edu>
2018-04-12 15:40:35 -0400

    new kappa/alpha

--- bench/bench.hpp
+++ bench/bench.hpp
@@ -31,8 +31,8 @@ void load_presets_by_host() {
     kappa = 40.0;
     update_size_ratio = 1.2;
   } else if (hostname == "aware.aladdin.cs.cmu.edu") {
-    kappa = 40.0;
-    update_size_ratio = 1.2;
+    kappa = 4.2;
+    update_size_ratio = 1.4;
   } else if (hostname == "beast") {
     kappa = 30.0;
     update_size_ratio = 1.2;
