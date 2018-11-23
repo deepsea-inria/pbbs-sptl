e56cbbcca3b7d34a027db6d9ee81292053595422
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-11-23 12:56:56 -0500

    use known revisions of non critical package dependencies

--- script/local-sources.nix
+++ script/local-sources.nix
@@ -2,13 +2,33 @@ let pkgs = import <nixpkgs> {}; in
 
 {
 
-  cmdlineSrc = ../../cmdline;
+  cmdlineSrc = pkgs.fetchFromGitHub {
+    owner  = "deepsea-inria";
+    repo   = "cmdline";
+    rev    = "c5f96b4aecb2019b5a690176195d37f7df3ed34b";
+    sha256 = "1rz9bfdd5242gy3vq4n9vj2rcr5pwp0j4cjycpn3pm7rnwrrcjnh";
+  };
 
-  cilkRtsSrc = ../../cilk-plus-rts-with-stats;
+  cilkRtsSrc = pkgs.fetchFromGitHub {
+    owner  = "deepsea-inria";
+    repo   = "cilk-plus-rts-with-stats";
+    rev    = "ae723bd498d9dd09dcfadefceca092d7e28d4352";
+    sha256 = "0bgsi9aqhjb80ds2c9mb1m3jzs6y3hwd2683f8gg301giifv17dn";
+  };
 
-  pbenchSrc = ../../pbench;
+  pbenchSrc = pkgs.fetchFromGitHub {
+    owner  = "deepsea-inria";
+    repo   = "pbench";
+    rev    = "6d862442713cae93153c439a3e5e5867e95af958";
+    sha256 = "1qdsl2amaqkckp2r2s4j5az8b0bpkbrgf8nc0zp575l1yjdzi2hh";
+  };
 
-  chunkedseqSrc = ../../chunkedseq;
+  chunkedseqSrc = pkgs.fetchFromGitHub {
+    owner  = "deepsea-inria";
+    repo   = "chunkedseq";
+    rev    = "d2925cf385fb43aff7eeb9c08cce88d321e5e02e";
+    sha256 = "09qyv48vb2ispl3zrxmvbziwf6vzjh3la7vl115qgmkq67cxv78b";
+  };
 
   sptlSrc = ../../sptl;
 
